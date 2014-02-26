7469c2e22937bb5320e23ea33b7ee72346cb4eae
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-02-26 18:06:59 +1100

    manged to read the file with space as I want to

--- process.toc.R
+++ process.toc.R
@@ -3,7 +3,19 @@ rm(list=ls(all=TRUE))
 # library
 source("functions/list_library.R")
 
-Mar13 <- read.table("toc.rowdata//210313 Lysimiter FACE (detailed).txt",
-                    header = TRUE, skip = 13, na.strings = c("NA"), fill = TRUE)
+Mar13 <- read.table("toc.rowdata//210313 Lysimiter FACE (detailed).txt", skip = 13,  fill = TRUE, sep = "\t",
+                    header = TRUE)
+  # sep="\t" enables you to read data with space withough separating them to different columns
 
-head(Mar13)
+# subset IC standard
+ICdata <- subset(Mar13, Anal. == "IC" & Type == "Standard")
+ICdata <- droplevels(ICdata)
+
+plot(Mean.Area ~ Conc., data = ICdata)
+
+# conc. = 10 is wrong so remove
+ICdata <- ICdata[ICdata$Conc. != 10.0, ]
+
+# calibration curve with intercept = 0
+ml <- lm(Mean.Area ~ Conc., data = ICdata)
+summary(ml)
