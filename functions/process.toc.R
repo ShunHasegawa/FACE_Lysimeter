c694256fe4a5f06cf738a26011676a6ecede9491
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-04-17 11:50:48 +1000

    unnecessary data is removed

--- functions/process.toc.R
+++ functions/process.toc.R
@@ -35,29 +35,60 @@ May13 <- correctIC("FACE.Lysimeter_02MAY2013")
 # format date
 May13$date <- as.Date(dmy(as.character(May13$Sample.Name)))
 
+###############
 # other files #
+###############
 files <- dir(path = "Data//TOC", pattern = "txt$", full.names = TRUE)
 
 
 ## Jul 2013 ##
-Jul13 <- read.table("Data//TOC/processed/040713 Lysimiter FACE.txt", skip = 11, fill = TRUE, sep = "\t", header = TRUE, 
+Jul13 <- read.table( "Data//TOC/FACE.Lysimeter_04JUL2013.txt", skip = 11, fill = TRUE, sep = "\t", header = TRUE, 
                 colClasses = c("Sample.ID" = "character"))
 
 # weird sample ID -> rename it
 Jul13$Sample.ID[grep("Untitle", Jul13$Sample.ID)] <- "1.1.S"
 
-jul13.data <- processTOC(Jul13)
+jul13.data <- processTOC(Jul13, DateCol = 3)
 
 
 ## Nov 2013 ##
-Nov13 <- read.table("Data//TOC/processed/141113 Lysimiter FACE.txt", skip = 11, fill = TRUE, sep = "\t", header = TRUE, 
+Nov13 <- read.table("Data//TOC/FACE.Lysimeter_14NOV2013.txt", skip = 11, fill = TRUE, sep = "\t", header = TRUE, 
                    colClasses = c("Sample.ID" = "character"))
 
-nov13.data <- processTOC(Nov13)
+nov13.data <- processTOC(Nov13, DateCol = 3)
+
+## Feb 2014 ##
+FEB14 <- read.table("Data//TOC/FACE.Lysimeter_25FEB2014.txt", skip = 11, fill = TRUE, sep = "\t", header = TRUE, 
+                    colClasses = c("Sample.ID" = "character"))
+feb14.data <- processTOC(FEB14, DateCol = 3)
+
+## Apr 2014 ##
+APR14 <- read.table("Data//TOC/FACE.Lysimeter_02APR2014.txt", skip = 11, fill = TRUE, sep = "\t", header = TRUE, 
+                    colClasses = c("Sample.ID" = "character", "Sample.Name" = "character"))
+apr14.data <- processTOC(APR14, DateCol = 4)
+names(apr14.data)[grep("Sample.Name", names(apr14.data))] <- "Sample.ID"
+
 
 # combine all files
-int.data <- rbind.fill(may13.data, mar13.data, jul13.data, nov13.data)
-int.data <- int.data[order(int.data$date, int.data$ring, int.data$plot, int.data$depth), ]
+int.data <- rbind.fill(Mar13, May13, jul13.data, nov13.data, feb14.data, apr14.data)
+
+# remove unnecessary rows and organize
+some(int.data)
+unique(int.data$Sample.ID)
+N.df <- subset(int.data, !(Sample.ID %in% c("Untitled", "")))
+unique(N.df$Sample.ID)
+
+# checked re-run sample 3.4.D on 4 Jul 2013
+N.df[N.df$Sample.ID == "3.4.D RERUN", ]
+N.df[N.df$date == as.Date("2013-07-04") & N.df$Sample.ID == "3.4.D", ]
+  ## there values are really close so just discard the Rerun data
+N.df <- N.df[-which(N.df$Sample.ID == "3.4.D RERUN"), ]
+
+# create ring, depth factor
+
+
+# order
+N.df <- N.df[order(N.df$date, N.df$ring, N.df$plot, N.df$depth), ]
 summary(int.data)
 unique(int.data$date)
 
