d68eaf2895d5fb1cecb4f1286743b01e9e4f1142
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-05-06 09:50:23 +1000

    new file "Stats.R" is created

---
+++ R/Stats.R
@@ -0,0 +1,24 @@
+###########
+# Nitrate #
+###########
+source("functions/nitrate.analysis.R")
+
+############
+# Ammonium #
+############
+source("functions/ammonium.analysis.R")
+
+#############
+# Phosphate #
+#############
+source("functions/phosphate.analysis.R")
+
+#######
+# TOC #
+#######
+source("functions/toc.analysis.R")
+
+######
+# TN #
+######
+source("functions/tn.analysis.R")
--- R/analysis.R
+++ R/analysis.R
@@ -42,31 +42,7 @@ source("functions/Lysimeter_summary_excel_table.R")
 #########
 # Stats #
 #########
-
-###########
-# Nitrate #
-###########
-source("functions/nitrate.analysis.R")
-
-############
-# Ammonium #
-############
-source("functions/ammonium.analysis.R")
-
-#############
-# Phosphate #
-#############
-source("functions/phosphate.analysis.R")
-
-#######
-# TOC #
-#######
-source("functions/toc.analysis.R")
-
-######
-# TN #
-######
-source("functions/tn.analysis.R")
+source("R/Stats.R")
 
 
 
