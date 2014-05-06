c859e327562ac8acf79996f15f3d16c72cf88c58
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-05-06 16:08:34 +1000

    fixed

--- R/Stats_ammonium_shallow.R
+++ R/Stats_ammonium_shallow.R
@@ -49,23 +49,30 @@ qqline(residuals.lm(Fml_S_pre))
 # Post-CO2 #
 ############
 
-range(lys$no[lys$depth == "shallow" & lys$post])
+range(lys$nh[lys$depth == "shallow" & lys$post])
 
-bxplts(value = "no", ofst= 0.003, data = subsetD(lys, depth == "shallow" & post))
-  # use box cox value
+bxplts(value = "nh", ofst= 0.06, data = subsetD(lys, depth == "shallow" & post))
+
+# remove the higher outlier
+boxplot(lys$nh[lys$depth == "shallow" & lys$post])
+NhRmOl <- subset(lys, nh < max(nh))
+bxplts(value = "nh", ofst= 0.06, data = subsetD(NhRmOl, depth == "shallow" & post))
+bxplts(value = "nh", ofst= 0.07, data = subsetD(NhRmOl, depth == "shallow" & post))
+ # use box-cox
+
 
 # different random factor structure
-m1 <- lme((no + .003)^(.061) ~ co2 * time, random = ~1|ring/plot, data = subsetD(lys, depth == "shallow" & post))
-m2 <- lme((no + .003)^(.061) ~ co2 * time, random = ~1|ring, data = subsetD(lys, depth == "shallow" & post))
-m3 <- lme((no + .003)^(.061) ~ co2 * time, random = ~1|id, data = subsetD(lys, depth == "shallow" & post))
+m1 <- lme((nh + .07)^(-0.34) ~ co2 * time, random = ~1|ring/plot, data = subsetD(NhRmOl, depth == "shallow" & post))
+m2 <- lme((nh + .07)^(-0.34) ~ co2 * time, random = ~1|ring, data = subsetD(NhRmOl, depth == "shallow" & post))
+m3 <- lme((nh + .07)^(-0.34) ~ co2 * time, random = ~1|id, data = subsetD(NhRmOl, depth == "shallow" & post))
 anova(m1, m2, m3)
-# m1 is better
+# m3 is better
 
 # autocorrelation
-atcr.cmpr(m1, rndmFac= "ring/plot")$models
-# model4 is best
+atcr.cmpr(m3, rndmFac= "id")$models
+# model3 is best
 
-Iml_S_post <- atcr.cmpr(m1, rndmFac= "ring/plot")[[4]]
+Iml_S_post <- atcr.cmpr(m3, rndmFac= "id")[[3]]
 
 # The initial model is: 
 Iml_S_post$call
@@ -74,7 +81,7 @@ Anova(Iml_S_post)
 
 # model simplification
 MdlSmpl(Iml_S_post)
-# CO2:time interactin and co2 are removed
+  # no factor is removed
 
 Fml_S_post <- MdlSmpl(Iml_S_post)$model.reml
 
@@ -85,9 +92,20 @@ Anova(Fml_S_post)
 
 # model diagnosis
 plot(Fml_S_post)
+  # wedge-shaped
 qqnorm(Fml_S_post, ~ resid(.)|id)
 qqnorm(residuals.lm(Fml_S_post))
 qqline(residuals.lm(Fml_S_post))
+  # not great....
+
+# Contrast
+cntrst<- contrast(Fml_S_post, 
+                  a = list(time = levels(NhRmOl$time[NhRmOl$post, drop = TRUE]), co2 = "amb"),
+                  b = list(time = levels(NhRmOl$time[NhRmOl$post, drop = TRUE]), co2 = "elev"))
+FACE_Lys_NH_S_postCO2_CntrstDf <- cntrstTbl(cntrst, data = subsetD(NhRmOl, depth == "shallow" & post), digit = 2)
+
+FACE_Lys_NH_S_postCO2_CntrstDf
+
 
 ## ----Stat_FACE_Lys_Ammonium_S_preCO2_Smmry
 # The initial model is:
