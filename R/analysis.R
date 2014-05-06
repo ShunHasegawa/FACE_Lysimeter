11dee55c07054bcc4418d8d01abee55d9e995027
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-05-06 10:44:59 +1000

    Old script is deleated

--- R/analysis.R
+++ R/analysis.R
@@ -26,32 +26,6 @@ source("R/functions.R")
 # load data
 load("output//data//FACE_lysimeter.RData")
 
-## split dataset to each nutrient & depth ##
-ntrs <- c("no", "nh", "po", "toc", "tn")
-
-lyses <- lapply(ntrs, function(x) ntr.splt (data = lys, ntr = x))
-
-no.d <- lyses[[1]]$deep
-no.s <- lyses[[1]]$shallow
-
-nh.d <- lyses[[2]]$deep
-nh.s <- lyses[[2]]$shallow
-
-po.d <- lyses[[3]]$deep
-po.s <- lyses[[3]]$shallow
-
-toc.d <- lyses[[4]]$deep
-toc.s <- lyses[[4]]$shallow
-
-tn.d <- lyses[[5]]$deep
-tn.s <- lyses[[5]]$shallow
-
-##########################
-# Summary table on excel #
-##########################
-ring.mean <- ddply(lys, .(time, date, ring, depth, co2), 
-                   function(x) colMeans(x[c("no", "nh", "po", "toc", "tc", "ic", "tn")], na.rm = TRUE))
-source("functions/Lysimeter_summary_excel_table.R")
 
 #########
 # Stats #
