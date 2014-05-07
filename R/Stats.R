a331dd461894996d5abdc187bf13fd76521bac47
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-05-07 12:37:14 +1000

    sourced

--- R/Stats.R
+++ R/Stats.R
@@ -1,24 +1,34 @@
 ###########
 # Nitrate #
 ###########
-source("R/nitrate.analysis.R")
+source("R/Stats_nitrate.R")
 
 ############
 # Ammonium #
 ############
-source("R/ammonium.analysis.R")
+source("R/Stats_ammonium.R")
 
 #############
 # Phosphate #
 #############
-source("R/phosphate.analysis.R")
+source("R/Stats_phosphate.R")
+
+######
+# TC #
+######
+source("R/Stats_TC.R")
 
 #######
 # TOC #
 #######
-source("R/toc.analysis.R")
+source("R/Stats_TOC.R")
+
+######
+# IC #
+######
+source("R/Stats_IC.R")
 
 ######
 # TN #
 ######
-source("R/tn.analysis.R")
+source("R/Stats_TN.R")
