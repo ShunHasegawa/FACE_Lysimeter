bc2a2efa84d88075506f5b68f3e3944cf8759af3
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-05-10 00:09:38 +1000

    leaching raito is tried but no indication of co2 effects

--- R/Stats_TOC.R
+++ R/Stats_TOC.R
@@ -18,34 +18,34 @@ lysMlt <- melt(lys, id = names(lys)[which(!(names(lys) %in% c("no", "nh", "po", 
 
 lysLeachMlt <- ddply(lysMlt, .(date, time, ring, plot, co2, time, pre, post, variable, id), 
                      summarise, 
-                     leach = value[depth == "shallow"] - value[depth == "deep"])
+                     leach = (value[depth == "shallow"] - value[depth == "deep"]) / value[depth == "shallow"])
 
 # remove NA
 lysLeachMlt <- lysLeachMlt[complete.cases(lysLeachMlt), ]
 
+# remove inf
+lysLeachMlt <- lysLeachMlt[!is.infinite(lysLeachMlt$leach), ]
+
 # cast
-head(lysLeachMlt)
 lysLeach <- cast(lysLeachMlt, date + time + ring + plot + co2 + pre + post + id ~ variable, value="leach")
-head(lysLeach)
 
 ###########
-# Pre-CO2 #
+# Post-CO2 #
 ###########
 lysLeachMlt
 range(lysLeach$toc[lysLeach$post], na.rm = TRUE)
 
-bxplts(value = "toc", data = subsetD(lysLeach, pre), ofst= 14)
-bxplts(value = "toc", data = subsetD(lysLeach, post), ofst= 14)
-
-
-# log seems better
+bxplts(value = "toc", data = subsetD(lysLeach, post), ofst= 2)
+bxcxplts(value = "toc", data = subsetD(lysLeach, post), sval= 1.8048, fval = 2)
+bxplts(value = "toc", data = subsetD(lysLeach, post), ofst= 1.8048)
+# box-cox lambda
 
 # different random factor structure
-m1 <- lme(sqrt(toc + 14) ~ co2 * time, random = ~1|ring/plot, data = subsetD(lysLeach, post), 
+m1 <- lme((toc + 1.8048)^2 ~ co2 * time, random = ~1|ring/plot, data = subsetD(lysLeach, post), 
           na.action = "na.omit")
-m2 <- lme(sqrt(toc + 14) ~ co2 * time, random = ~1|ring, data = subsetD(lysLeach, post),
+m2 <- lme((toc + 1.8048)^2 ~ co2 * time, random = ~1|ring, data = subsetD(lysLeach, post),
           na.action = "na.omit")
-m3 <- lme(sqrt(toc + 14) ~ co2 * time, random = ~1|id, data = subsetD(lysLeach, post),
+m3 <- lme((toc + 1.8048)^2 ~ co2 * time, random = ~1|id, data = subsetD(lysLeach, post),
           na.action = "na.omit")
 anova(m1, m2, m3)
 # m3 is slightly better
@@ -53,9 +53,9 @@ anova(m1, m2, m3)
 # autocorrelation
 atml <- atcr.cmpr(m3, rndmFac= "id")
 atml$models
-# model1 is best
+# model5 is best
 
-Iml_Leach_post <- atml[[1]]
+Iml_Leach_post <- atml[[5]]
 
 # The initial model is: 
 Iml_Leach_post$call
