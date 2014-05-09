c29ca84d2ca638e1023741457d522f7bdbc8f255
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-05-09 23:49:27 +1000

    dataset for leaching is created

--- R/Stats_TOC.R
+++ R/Stats_TOC.R
@@ -7,3 +7,26 @@ source("R/Stats_TOC_shallow.R")
 # Deep #
 ########
 source("R/Stats_TOC_deep.R")
+
+############
+# Leaching #
+############
+summary(lys)
+
+# melt
+lysMlt <- melt(lys, id = names(lys)[which(!(names(lys) %in% c("no", "nh", "po", "toc", "tc", "ic", "tn")))])
+
+lysLeachMlt <- ddply(lysMlt, .(date, time, ring, plot, co2, time, pre, post, variable), 
+                     summarise, 
+                     leach = value[depth == "shallow"] - value[depth == "deep"])
+
+# remove NA
+lysLeachMlt <- lysLeachMlt[complete.cases(lysLeachMlt), ]
+
+# cast
+head(lysLeachMlt)
+lysLeach <- cast(lysLeachMlt, date + time + ring + plot + co2 + pre + post ~ variable, value="leach")
+head(lysLeach)
+
+
+
