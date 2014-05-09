107776fd493be557d1a198ea7e71426a961e3629
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-05-09 12:46:36 +1000

    xtable disattached

--- R/Stats_TC_shallow.R
+++ R/Stats_TC_shallow.R
@@ -109,11 +109,11 @@ qqline(residuals.lm(Fml_S_post))
 ## ----Stat_FACE_Lys_TC_S_preCO2_Smmry
 # The initial model is:
 Iml_S_pre$call
-xtable(Anova(Iml_S_pre), floating = FALSE)
+Anova(Iml_S_pre)
 
 # The final model is :
 Fml_S_pre$call
-xtable(Anova(Fml_S_pre), floating = FALSE)
+Anova(Fml_S_pre)
 
 ## ----Stat_FACE_Lys_TC_S_postCO2_Smmry
 # The initial model is:
