25af56368be49ec02370d0ce8421c541f6358967
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-08-21 02:00:10 +0100

    fig for manuscript is created

--- R/Figs.R
+++ R/Figs.R
@@ -31,9 +31,9 @@ p <- PltCO2Mean(TrtMean) +
   facet_wrap( ~variable, ncol = 2, scale = "free_y")
 
 # modify labels
-ylabs <- c(expression(NO[3]^"-"-N),
-           expression(NH[4]^"+"-N),
-           expression(PO[4]^"3-"-P),
+ylabs <- c(expression(NO[3]^"-"),
+           expression(NH[4]^"+"),
+           expression(PO[4]^"3-"),
            expression(DOC),
            expression(TC),
            expression(IC),
@@ -43,3 +43,52 @@ pl <- facet_wrap_labeller(p, labels = ylabs)
 
 
 ggsavePP(filename = "output/figs/FACE_LysimeterCO2", plot = pl, width = 8, height = 8)
+
+#######################
+# Figs for manuscript #
+#######################
+science_theme <- theme(panel.grid.major = element_line(size = 0.2, color = "grey"), 
+                       axis.text.x  = element_text(angle=45, vjust= 1, hjust = 1),
+                       legend.position = "top",
+                       legend.title = element_blank())
+
+# subset Nitrate, ammonium, phosphate and TOC
+df <- subsetD(TrtMean, variable %in% c("no", "nh", "po", "toc"))
+df$grp <- df$co2:df$depth
+legLab <- c("Ambient-Shallow", "Ambient-Deep", expression(eCO[2]-Shallow), expression(eCO[2]-Deep))
+
+p <- ggplot(df, aes(x = date, y = Mean, group = grp))
+
+p2 <- p + geom_line(aes(linetype = grp), alpha = .8) + 
+  geom_errorbar(aes(ymin = Mean - SE, ymax = Mean + SE), 
+                width = 15, size = .3,
+                position = position_dodge(20)) + 
+  geom_point(aes(shape = grp, fill = grp), position = position_dodge(20), alpha = .8) +
+  labs(x = "Month", y = expression(Dissolved~nutrients~'in'~soil~water~(mg~l^"-1"))) +
+  geom_vline(xintercept = as.numeric(as.Date("2012-09-18")), 
+             linetype = "dashed", col = "black") +
+  scale_x_date(breaks= date_breaks("2 month"),
+               labels = date_format("%b-%y"),
+               limits = as.Date(c("2012-7-1", "2014-4-2"))) +
+  scale_shape_manual(values = c(21:24), labels = legLab) +
+  scale_fill_manual(values = rep(c("black", "white"), each = 2), labels = legLab) +
+  scale_linetype_manual(values = rep(c("solid", "dashed"), 2), labels = legLab) +
+  facet_wrap( ~variable, ncol = 2, scale = "free_y") +
+  science_theme
+
+# modify labels
+ylabs <- c(expression(NO[3]^"-"),
+           expression(NH[4]^"+"),
+           expression(PO[4]^"3-"),
+           expression(DOC))
+
+pl <- facet_wrap_labeller(p2, labels = ylabs)
+ggsavePP(filename = "output//figs/FACE_Manuscript/FACE_Lysimeter", plot = pl, width = 7, height = 6)
+
+
+# white-black figure
+WBFig <- function(data, ylab, facetLab = ylab_label, figTheme = science_theme){
+  
+  return(p2)
+}
+
