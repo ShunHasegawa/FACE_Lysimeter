0c31eeef383f0869ed8335b56c871d5ce2f96613
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-09-08 16:28:54 +0100

    steplmer is created

--- R/functions.R
+++ R/functions.R
@@ -404,7 +404,7 @@ bxplts <- function(value, ofst = 0, data, ...){
   data$y <- data[[value]] + ofst #ofst is added to make y >0
   a <- boxcox(y ~ co2 * time, data = data, ...)
   par(mfrow = c(2, 3))
-  boxplot(y ~ co2*time, data, main = "row")
+  boxplot(y ~ co2*time, data, main = "raw")
   boxplot(log(y) ~ co2*time, main = "log", data)
   boxplot(sqrt(y) ~ co2*time, main = "sqrt", data)
   boxplot(y^(1/3) ~ co2*time, main = "power(1/3)", data)
@@ -545,3 +545,15 @@ printLysCO2Tbl <- function(data, variable, datatype,  ...){
               ...)
 }
 
+###########################
+# step deletion with lmer #
+###########################
+stepLmer <- function(model, red.rndm = FALSE, ddf = "Kenward-Roger", ...){
+  require(lmerTest)
+  update(step(model, reduce.random = red.rndm, ddf = ddf,...)$model, 
+         contrasts = NULL)
+}
+# use "Kenward-Roger" for approximation for denominator degrees of freedom. This
+# is the same as the default DF given by Anova(model, test.statistic = "F). The
+# default of step gives me a warning message for IEM-NO3 for some reasons (not
+# sure why.. so changed it.)
