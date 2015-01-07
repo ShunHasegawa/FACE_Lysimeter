7d9bbb82616133248c051a12e0dc7da44b9a4738
ShunHasegawa <shun.hasegawa10@imperial.ac.uk>
2015-01-07 09:57:05 +0000

    P>F -> P

--- R/functions.R
+++ R/functions.R
@@ -603,8 +603,8 @@ StatTable <- function(x, variable) { # x is anova result
   
   # add a row for column name of the table in the fig 
   df <- rbind(df, data.frame(predictor = "", 
-                             stars = "italic('P>F')", 
-                             p = "italic('P>F')"))
+                             stars = "italic('F')", 
+                             p = "italic('F')"))
   
   result <- merge(df, data.frame(predictor = c("co2", "time", "co2:time")), all = TRUE)
   
